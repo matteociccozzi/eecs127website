aaa15e3285af258a60ec877a432e76aaa19aff93
Suvansh Sanjeev <suvansh@berkeley.edu>
2020-04-07 00:45:03 -0700

    deploy

--- gradeBreakdown.js
+++ gradeBreakdown.js
@@ -5,24 +5,6 @@ Chart.defaults.global.responsive = true;
 var colors = ["#F7464A", "#46BFBD", "#FDB45C", "#BF7130", "#39E639"]
 var highlights = ["#FF5A5E", "#5AD3D1", "#FFC870", "#BF8140", "#49E649"]
 var data = [
-  {
-    value: "30",
-    color: colors[0],
-    highlight: highlights[0],
-    label: "Homework"
-  },
-  {
-    value: "30",
-    color: colors[1],
-    highlight: highlights[1],
-    label: "Midterm"
-  },
-  {
-    value: "40",
-    color: colors[2],
-    highlight: highlights[2],
-    label: "Final"
-  }
 ]
 var ctx = document.getElementById("gradeBreakdown").getContext("2d");
 var myNewChart = new Chart(ctx).Doughnut(data, []);
